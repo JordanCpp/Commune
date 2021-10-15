88b90859d08906f60f637729889f48ac2e0e6e07
Евгений <jordan631@yandex.ru>
2021-10-15 11:00:54 +0400

    ProtoManager и Stat

---
+++ Source/Fallout/Gameplay/Stat.h
@@ -0,0 +1,18 @@
+#ifndef _Fallout_Gameplay_Stat_h_
+#define _Fallout_Gameplay_Stat_h_
+
+#include <array>
+
+namespace Fallout
+{
+    namespace Gameplay
+    {
+        class Stat
+        {
+        public:
+        private:
+        };
+    }
+}
+
+#endif
---
+++ Source/Fallout/Managers/ProtoManager.h
@@ -0,0 +1,18 @@
+#ifndef _Fallout_Managers_ProtoManager_h_
+#define _Fallout_Managers_ProtoManager_h_
+
+#include "../../Engine/Managers/XmlManager.h"
+
+namespace Fallout
+{
+    namespace Managers
+    {
+        class ProtoManager
+        {
+        public:
+        private:
+        };
+    }
+}
+
+#endif
